95ce785d5ca74e1dd76e9f374f61fc9e913b3e4c
KateBarkova <barkova.kate@yandex.ru>
2018-06-06 15:35:08 +0300

    Учебный проект Холст - добавление облака и гистограммы

---
+++ js/stat.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 20;
+var BAR_WIDTH = 40;
+var BAR_GAP = 40;
+var CHART_HEIGHT = 150;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP + FONT_GAP*4, FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP + FONT_GAP*4, FONT_GAP*2);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    var barHeight = (times[i] * CHART_HEIGHT) / maxTime;
+
+    ctx.textBaseline = 'bottom';
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH)*i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
+    ctx.fillText(Math.floor (times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH)*i, CLOUD_Y + CLOUD_HEIGHT - barHeight - FONT_GAP*2.5);
+    var randomColor = 'hsl(240, ' + Math.floor(Math.random()*70 + 30) + '%, 50%)';
+    console.log(randomColor);
+    ctx.fillStyle = randomColor;
+    if (players[i] == 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    }
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH)*i, CLOUD_HEIGHT - BAR_GAP - barHeight, BAR_WIDTH, barHeight);
+  }
+};
+
